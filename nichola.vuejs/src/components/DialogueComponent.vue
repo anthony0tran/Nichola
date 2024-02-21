c0a580c707eaf080e80fc2c0df94177eccef4628
anthony0tran <anthony0tran@gmail.com>
2024-02-21 22:59:35 +0100

    added DialogueComponent and TextOutputComponent

---
+++ nichola.vuejs/src/components/DialogueComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main id="dialogueContainer">Dialogue options go here</main>
+</template>
+
+<style scoped>
+#dialogueContainer {
+    border-color: black;
+    border-style: solid;
+    border-radius: 8px;
+    position: relative;
+    font-size: 50px;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 5px 0px 10px 15px;
+}
+</style>
